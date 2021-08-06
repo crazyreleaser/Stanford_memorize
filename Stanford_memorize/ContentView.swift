b7462ad31c365562904ad04c79c592c77af7685a
crazyreleaser <crazyreleaser@yandex.ru>
2021-08-06 17:56:06 +0300

    Initial Commit

---
+++ Stanford_memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stanford_memorize
+//
+//  Created by admin on 06.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stanford_memorize/Stanford_memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_memorizeApp.swift
+//  Stanford_memorize
+//
+//  Created by admin on 06.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
