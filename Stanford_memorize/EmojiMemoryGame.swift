1c01524252842f5ecfabee8607d5546fd440f183
crazyreleaser <crazyreleaser@yandex.ru>
2021-08-16 12:51:31 +0300

    Extremly simlifying the code in EmojiMemoryGame - so strange...

--- Stanford_memorize/EmojiMemoryGame.swift
+++ Stanford_memorize/EmojiMemoryGame.swift
@@ -13,7 +13,18 @@ func makeCardContent(index: Int) -> String {
 }
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4, createCardContent: makeCardContent)
+    // This is swift extremly simplify the code thats may cause some missunderstanding
+    
+//    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4, createCardContent: makeCardContent)
+//    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4, createCardContent: (index: Int) -> String {
+//        return "smile"
+//    })
+//    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4, createCardContent: { index in
+//        return "smile"
+//    })
+//    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4, createCardContent: { index in "smile" })
+//    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4) { index in "smile" }
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOFPairsOfCards: 4) { _ in "smile" }
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
